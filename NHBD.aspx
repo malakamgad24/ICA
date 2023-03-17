5b0c395030948dd8680071770abe552041af497f
malakamgad24 <128064324+malakamgad24@users.noreply.github.com>
2023-03-18 00:12:59 +0200

    Add files via upload

---
+++ NHBD.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NHBD.aspx.cs" Inherits="NHBD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" method="get">
+        <div>
+            Name:
+            <asp:TextBox ID="NHBD_TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Gender:
+            <asp:RadioButton ID="NHBD_RadioButton1" runat="server" GroupName="my"/>
+            Male
+            <asp:RadioButton ID="NHBD_RadioButton2" runat="server" GroupName="my"/>
+            Female
+            <br />
+            Password:
+            <asp:TextBox ID="NHBD_TextBox2" TextMode= "Password" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="NHBD_Button1" runat="server" Text="Submit" onclick="NHBD_Button1_click"/>
+            <br />
+            <asp:Label ID="NHBD_Label1" runat="server" Text="Label"></asp:Label>
+      
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ NHBD.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NHBD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void NHBD_Button1_click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(NHBD_TextBox1.Text))
+            {
+                if (NHBD_RadioButton1.Checked == true)
+                {
+                    NHBD_Label1.Text = "Thank you Mr.  " +
+                    Request.QueryString["NHBD_TextBox1"] + "." + " Your Registration has been successfully completed." +
+                    "<br/> <br/> The URL is: " + Request.Url.ToString();
+                }
+                if (NHBD_RadioButton2.Checked == true)
+                {
+                    NHBD_Label1.Text = "Thank you Ms.  " +
+                    Request.QueryString["NHBD_TextBox1"] + "." + " Your Registration has been successfully completed." +
+                    "<br/> <br/> The URL is: " + Request.Url.ToString();
+                }
+            }
+
+        }
+    }
+}
